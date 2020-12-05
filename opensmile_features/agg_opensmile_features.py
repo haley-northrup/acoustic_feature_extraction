0dad9340142d02c0174c103426b1ce8f940aa847
Haley Northrup <hnorthru@umich.edu>
2020-12-05 14:43:20 -0500

    add inputs to agg file

--- opensmile_features/agg_opensmile_features.py
+++ opensmile_features/agg_opensmile_features.py
@@ -1,15 +1,38 @@
 import os 
 import pandas as pd 
+import argparse
 
-#TODO add metadata columns (call_id, subject_id, date) 
+from IPython import embed 
 
-input_dir = '/scratch/emilykmp_root/emilykmp/aromana/gemaps_features/days'
-output_dir = '/nfs/turbo/chai-health/aromana/gemaps_features/'
-files = os.listdir(input_dir) 
+#Parse Arguments 
+#**********************
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_dir', type=str, help="Path to directory with individual output files")
+parser.add_argument('--output_dir', type=str, help="Path to directory to output feature files to.")
+parser.add_argument('--level', type=str, default='call')
+parser.add_argument('--call_type', type=str, default='all')
+args = parser.parse_args()
+
+#Aggregate individual files in input_dir 
+#***********************************
+files = os.listdir(args.input_dir) 
 df_full = pd.DataFrame()
 for f in files: 
-    df = pd.read_csv(os.path.join(input_dir, f), index_col=0, header=0, names=[f.strip('.csv')])
+    df = pd.read_csv(os.path.join(args.input_dir, f), index_col=0, header=0, names=[f.strip('.csv')])
     df_full = df_full.append(df.T) 
-df_full.to_csv(os.path.join(output_dir, 'day_gemaps.csv')) 
+
+#Add metadata based on level 
+if args.level == 'day': 
+        df_full['day_id'] = df_full.index 
+        sub_ids = df_full['day_id'].apply(lambda x: x.split('_')[0]).values 
+        dates = df_full['day_id'].apply(lambda x: x.split('_')[1]).values
+        df_full.insert(loc=0, column='subject_id', value=sub_ids)
+        df_full.insert(loc=1, column='date', value=dates) 
+        df_full = df_full.drop(['day_id'], axis=1) 
+elif args.level == 'call':
+        df_full.insert(loc=0, column='call_id', value=df_full.index.values) 
+
+#Save aggregate file 
+df_full.to_csv(os.path.join(args.output_dir, args.level + '_' + args.call_type + '_gemaps.csv'), index=False) 
 
 
--- opensmile_features/run_gemaps_features.sh
+++ opensmile_features/run_gemaps_features.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
-####SBATCH --job-name=priori_day
-####SBATCH --mail-user=hnorthru@umich.edu
-####SBATCH --mail-type=FAIL
-####SBATCH --nodes=1
-####SBATCH --ntasks-per-node=1
-####SBATCH --mem=10000
-####SBATCH --time=06:00:00
-####SBATCH --array 1-100
-####SBATCH --output=logs/%x-%j.log
-####SBATCH --account=emilykmp1
-####SBATCH --partition=standard
-####SBATCH --export=NONE
+#####SBATCH --job-name=priori_day
+#####SBATCH --mail-user=hnorthru@umich.edu
+#####SBATCH --mail-type=FAIL
+#####SBATCH --nodes=1
+#####SBATCH --ntasks-per-node=1
+#####SBATCH --mem=10000
+#####SBATCH --time=06:00:00
+#####SBATCH --array=1-100
+#####SBATCH --output=logs/%x-%j.log
+#####SBATCH --account=emilykmp1
+#####SBATCH --partition=standard
+#####SBATCH --export=NONE 
 
 module load python-anaconda3
 
 python3 get_opensmile_features.py \
---job_num=0  \
---level='day' \
+--level='call' \
+--job_num=$SLURM_ARRAY_TASK_ID  \
 --call_type='all' \
 --segments_dir='/nfs/turbo/McInnisLab/priori_v1_data/segments_all/wav/' \
---output_dir='/scratch/emilykmp_root/emilykmp/hnorthru/gemaps_features/' \
---metadata_path='/nfs/turbo/McInnisLab/priori_v1_data/segments_all/data.csv' \
+--output_dir='/scratch/emilykmp_root/emilykmp/aromana/gemaps_features/' \
+--metadata_path='/nfs/turbo/McInnisLab/priori_v1_data/collections/emotion_preds.csv' \
 --config_path='/nfs/turbo/McInnisLab/Libraries/opensmile-2.3.0/config/gemaps/eGeMAPSv01a.conf' \
 
-#wait 
-#python3 aggregate_gemaps_features.py
+wait 
 
-#--job_num=$SLURM_ARRAY_TASK_ID#
- 
+python3 agg_opensmile_features.py \
+--level='call' \
+--call_type='all' \
+--input_dir='/scratch/emilykmp_root/emilykmp/aromana/gemaps_features/calls/all/' \
+--output_dir='.' \
