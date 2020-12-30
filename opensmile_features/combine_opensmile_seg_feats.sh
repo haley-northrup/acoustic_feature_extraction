42bb427cd3bd09a8667f41702631a4b0f993a626
Haley Northrup <hnorthru@umich.edu>
2020-12-30 17:21:18 -0500

    update settings

--- opensmile_features/combine_opensmile_seg_feats.sh
+++ opensmile_features/combine_opensmile_seg_feats.sh
@@ -5,14 +5,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=10000
-#SBATCH --time=06:00:00
+#SBATCH --time=10:00:00
 #SBATCH --array 1
 #SBATCH --output=logs/%x-%j.log
 #SBATCH --account=emilykmp1
 #SBATCH --partition=standard
 #SBATCH --export=NONE
 
-python3 agg_opensmile_segments.py \
+python3 combine_opensmile_seg_feats.py \
 --level='segment' \
 --call_type='all' \
 --input_dir='/scratch/emilykmp_root/emilykmp/aromana/gemaps_features/segments/' \
--- opensmile_features/run_gemaps_features.sh
+++ opensmile_features/run_gemaps_features.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-#####SBATCH --job-name=priori_day
-#####SBATCH --mail-user=hnorthru@umich.edu
-#####SBATCH --mail-type=FAIL
-#####SBATCH --nodes=1
-#####SBATCH --ntasks-per-node=1
-#####SBATCH --mem=10000
-#####SBATCH --time=06:00:00
-#####SBATCH --array=1-100
-#####SBATCH --output=logs/%x-%j.log
-#####SBATCH --account=emilykmp1
-#####SBATCH --partition=standard
-#####SBATCH --export=NONE 
+#SBATCH --job-name=priori_day
+#SBATCH --mail-user=hnorthru@umich.edu
+#SBATCH --mail-type=FAIL
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=10000
+#SBATCH --time=06:00:00
+#SBATCH --array=1-100
+#SBATCH --output=logs/%x-%j.log
+#SBATCH --account=emilykmp1
+#SBATCH --partition=standard
+#SBATCH --export=NONE 
 
 module load python-anaconda3
 
 python3 get_opensmile_features.py \
---level='call' \
+--level='day' \
 --job_num=$SLURM_ARRAY_TASK_ID  \
---call_type='all' \
+--call_type='personal' \
 --segments_dir='/nfs/turbo/McInnisLab/priori_v1_data/segments_all/wav/' \
 --output_dir='/scratch/emilykmp_root/emilykmp/aromana/gemaps_features/' \
 --metadata_path='/nfs/turbo/McInnisLab/priori_v1_data/collections/emotion_preds.csv' \
@@ -26,7 +26,7 @@ python3 get_opensmile_features.py \
 wait 
 
 python3 agg_opensmile_features.py \
---level='call' \
---call_type='all' \
---input_dir='/scratch/emilykmp_root/emilykmp/aromana/gemaps_features/calls/all/' \
+--level='day' \
+--call_type='personal' \
+--input_dir='/scratch/emilykmp_root/emilykmp/aromana/gemaps_features/days/personal/' \
 --output_dir='.' \
