76a439afc5faa3c008bc345e1d56ecaeacc779fd
Haley Northrup <hnorthru@umich.edu>
2021-01-07 15:05:58 -0500

    update settings

--- rhythm_features/get_rhythm_features.sh
+++ rhythm_features/get_rhythm_features.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#SBATCH --job-name=rhythm_feats 
+#SBATCH --job-name=rhythm_day 
 #SBATCH --mail-user=hnorthru@umich.edu
 #SBATCH --mail-type=FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=10000
-#SBATCH --time=06:00:00
+#SBATCH --time=12:00:00
 #SBATCH --array=1-100
 #SBATCH --output=logs/%x-%j.log
 #SBATCH --account=emilykmp1
@@ -18,15 +18,15 @@ conda activate gid_8_tn_20200220
 python3 get_rhythm_features.py \
 --job_num=$SLURM_ARRAY_TASK_ID   \
 --level='day' \
---call_type='assessment' \
+--call_type='personal' \
 --segments_dir='/nfs/turbo/McInnisLab/PRIORI_v1_Microsoft_Azure/PRIORI-v1-Microsoft-segments/wav/' \
---output_dir='./temp/feats/' \
+--output_dir='/scratch/emilykmp_root/emilykmp/hnorthru/priori_v1/microsoft_rhythm/' \
 --metadata_path='/nfs/turbo/McInnisLab/PRIORI_v1_Microsoft_Azure/PRIORI-v1-Microsoft-segments/priori_v1_ma_segments.csv' \
 
 wait 
 
 python3 agg_rhythm_features.py \
 --level='day' \
---call_type='assessment' \
---input_dir='./temp/feats/days/assessment/' \
---output_dir='./temp/feats_agg/' \
+--call_type='personal' \
+--input_dir='/scratch/emilykmp_root/emilykmp/hnorthru/priori_v1/microsoft_rhythm/day/personal/' \
+--output_dir='.' \
